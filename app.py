2b006f205314a11820730984a343051430d2c4e4
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-04-13 13:55:38 -0700

    Adding some error checking for the sports

--- app.py
+++ app.py
@@ -5,16 +5,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = [
+    "Dodgeball",
+    "Flag Footall",
+    "Baseball",
+    "Basketball"
+]
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports= SPORTS)
 
 
 @app.route("/register", methods=["POST"])
 def register():
     # checking for the name
-    if not request.form.get("name") or not request.form.get("sport"):
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
         return render_template("failure.html")
     return render_template("register.html")
